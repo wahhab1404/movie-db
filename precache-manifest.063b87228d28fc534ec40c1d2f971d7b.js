bb67d3af8943d9acb2892070d39bcec9d89b453b
wahhab1404 <a.wahhab1404@gmail.com>
2019-11-06 16:17:09 +0300

    Updates

---
+++ precache-manifest.063b87228d28fc534ec40c1d2f971d7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cf10de4ee978384994cf3dc1546c00d",
+    "url": "/movie-db/index.html"
+  },
+  {
+    "revision": "c367f6e0405fac4fc3dc",
+    "url": "/movie-db/static/css/main.ee98c097.chunk.css"
+  },
+  {
+    "revision": "80d0fd426c304ab039eb",
+    "url": "/movie-db/static/js/2.bc39bb8c.chunk.js"
+  },
+  {
+    "revision": "c367f6e0405fac4fc3dc",
+    "url": "/movie-db/static/js/main.71847582.chunk.js"
+  },
+  {
+    "revision": "28bc7c70dc45fea09cc4",
+    "url": "/movie-db/static/js/runtime-main.84f1c5bc.js"
+  }
+]);
